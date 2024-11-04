19b32555d62d0bfe38e1c212289c2ce8bd16a010
sysops-lab <omondi.omollo.ao@gmail.com>
2024-11-04 16:02:27 +0300

    First career page

--- FlaskApp.py
+++ FlaskApp.py
@@ -8,6 +8,13 @@ def Hello():
 
     return "Hello, This is my first application in Flask"
 
+
+@app.route("/careers") 
+
+def careers():
+
+    return "This is my first career page"
+
 if __name__ == "__main__":
 
     app.run(debug=True)
